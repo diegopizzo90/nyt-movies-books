20a790af45043c6f87d4df88bcb01803dbd55c1a
Diego Pizzo <pizzodi@gmail.com>
2017-11-18 22:53:19 +0100

    move MainActivity into ui package

--- nyt-movies-books/app/src/main/java/com/diegopizzo/moviesbooks/ui/MainActivity.java
+++ nyt-movies-books/app/src/main/java/com/diegopizzo/moviesbooks/ui/MainActivity.java
@@ -1,12 +1,14 @@
-package com.diegopizzo.moviesbooks;
+package com.diegopizzo.moviesbooks.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.diegopizzo.moviesbooks.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
