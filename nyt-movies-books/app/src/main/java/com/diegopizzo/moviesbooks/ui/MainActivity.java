61f67a3faf0f3e85f77d67484623ec41581ad4d6
Diego Pizzo <pizzodi@gmail.com>
2017-11-19 12:26:27 +0100

    implements interface for interaction with fragments

--- nyt-movies-books/app/src/main/java/com/diegopizzo/moviesbooks/ui/MainActivity.java
+++ nyt-movies-books/app/src/main/java/com/diegopizzo/moviesbooks/ui/MainActivity.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.diegopizzo.moviesbooks.R;
+import com.diegopizzo.moviesbooks.ui.booksfragment.BooksFragment;
+import com.diegopizzo.moviesbooks.ui.moviesreviewsfragment.MoviesReviewsFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MoviesReviewsFragment.OnFragmentInteractionListener, BooksFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
