7f28b33668a565c5847bf8ce9ac89be3eafc73f9
Diego Pizzo <pizzodi@gmail.com>
2017-11-22 12:39:36 +0100

    implemented the recycler view with endless scroll

---
+++ nyt-movies-books/app/src/main/java/com/diegopizzo/moviesbooks/ui/moviesreviewsfragment/MoviesReviewsAdapter.java
@@ -0,0 +1,101 @@
+package com.diegopizzo.moviesbooks.ui.moviesreviewsfragment;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.diegopizzo.moviesbooks.R;
+import com.diegopizzo.moviesbooks.business.network.model.movies.Multimedia;
+import com.diegopizzo.moviesbooks.business.network.model.movies.Result;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by diegopizzo on 20/11/2017.
+ */
+
+public class MoviesReviewsAdapter extends RecyclerView.Adapter<MoviesReviewsAdapter.ViewHolder> {
+
+    // Store the context for easy access
+    private final Context mContext;
+    // Store a member variable for the items
+    private final List<Result> resultList;
+
+    public MoviesReviewsAdapter(final Context mContext) {
+        resultList = new ArrayList<>();
+        this.mContext = mContext;
+    }
+
+    // Usually involves inflating a layout from XML and returning the holder
+    @Override
+    public ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
+
+        final Context context = parent.getContext();
+        final LayoutInflater inflater = LayoutInflater.from(context);
+        // Inflate the custom layout
+        final View repoView = inflater.inflate(R.layout.movie_review_item, parent, false);
+
+        // Return a new holder instance
+        return new ViewHolder(repoView);
+    }
+
+    // Involves populating data into the item through holder
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        // Get the data model based on position
+        final Result result = resultList.get(position);
+        // Set item views based on your views and data model
+        final TextView titleTextView = holder.title;
+        titleTextView.setText(result.getDisplayTitle());
+        final TextView bylineTextView = holder.byline;
+        bylineTextView.setText(result.getByline());
+        final TextView headlineTextView = holder.healine;
+        headlineTextView.setText(result.getHeadline());
+        final ImageView movieImageView = holder.movieImage;
+        if (result.getMultimedia() != null) {
+            final Multimedia multimedia = result.getMultimedia();
+            Glide.with(mContext).load(multimedia.getSrc()).into(movieImageView);
+            movieImageView.setMinimumHeight(multimedia.getHeight());
+            movieImageView.setMinimumWidth(multimedia.getWidth());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return resultList.size();
+    }
+
+    public void swapItems(final List<Result> results) {
+        final int curSize = resultList.size();
+        resultList.addAll(results);
+        notifyItemRangeChanged(curSize, resultList.size() - 1);
+    }
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+        TextView title;
+        TextView byline;
+        TextView healine;
+        ImageView movieImage;
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each subview
+        public ViewHolder(final View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.movieTitleTextView);
+            byline = (TextView) itemView.findViewById(R.id.movieBylineTextView);
+            healine = (TextView) itemView.findViewById(R.id.movieHeadlineTextView);
+            movieImage = (ImageView) itemView.findViewById(R.id.movieImageView);
+        }
+    }
+}
--- nyt-movies-books/app/src/main/java/com/diegopizzo/moviesbooks/ui/moviesreviewsfragment/MoviesReviewsFragment.java
+++ nyt-movies-books/app/src/main/java/com/diegopizzo/moviesbooks/ui/moviesreviewsfragment/MoviesReviewsFragment.java
@@ -2,10 +2,21 @@ package com.diegopizzo.moviesbooks.ui.moviesreviewsfragment;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.OrientationHelper;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.diegopizzo.moviesbooks.R;
+import com.diegopizzo.moviesbooks.business.network.model.movies.Movies;
 import com.diegopizzo.moviesbooks.config.MoviesBooksApplication;
 import com.diegopizzo.moviesbooks.config.mvp.AbstractMvpFragment;
+import com.diegopizzo.moviesbooks.ui.EndlessRecyclerViewScrollListener;
+
+import butterknife.BindView;
 
 /**
  * Created by diegopizzo on 18/11/2017.
@@ -16,7 +27,12 @@ public class MoviesReviewsFragment extends AbstractMvpFragment<MoviesReviewsFrag
 
     public static final String TAG = "MoviesReviewsFragment";
     public static final String TITLE = "Movies Reviews";
+    private static final String BUNDLE_MOVIES_LIST = "bundleMoviesList";
+    @BindView(R.id.moviesReclyclerView)
+    RecyclerView moviesReclyclerView;
     private OnFragmentInteractionListener onFragmentInteractionListener;
+    private EndlessRecyclerViewScrollListener scrollListener;
+    private MoviesReviewsAdapter moviesReviewsAdapter;
 
     public static MoviesReviewsFragment newInstance(final Bundle bundle) {
         final MoviesReviewsFragment moviesReviewsFragment = new MoviesReviewsFragment();
@@ -24,6 +40,54 @@ public class MoviesReviewsFragment extends AbstractMvpFragment<MoviesReviewsFrag
             moviesReviewsFragment.setArguments(bundle);
         }
         return moviesReviewsFragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container, final Bundle savedInstanceState) {
+        final View view = super.onCreateView(inflater, container, savedInstanceState);
+
+        setRecyclerView();
+        presenter.moviesReviews(0);
+
+        return view;
+    }
+
+    @Override
+    public void onSaveInstanceState(final Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //outState.putParcelableArrayList(BUNDLE_MOVIES_LIST, (ArrayList<? extends Parcelable>) results);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable final Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    private void setRecyclerView() {
+        // First param is number of columns and second param is orientation i.e Vertical or Horizontal
+        final StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2,
+                OrientationHelper.VERTICAL);
+        // Attach the layout manager to the recycler view
+        moviesReclyclerView.setLayoutManager(staggeredGridLayoutManager);
+        moviesReviewsAdapter = new MoviesReviewsAdapter(getContext());
+        moviesReclyclerView.setAdapter(moviesReviewsAdapter);
+
+        scrollListener = new EndlessRecyclerViewScrollListener(staggeredGridLayoutManager) {
+            @Override
+            public void onLoadMore(final int page, final int totalItemsCount, final RecyclerView view) {
+                presenter.moviesReviews(totalItemsCount);
+            }
+        };
+
+        moviesReclyclerView.addOnScrollListener(scrollListener);
+    }
+
+    @Override
+    public void setDataOnRecyclerView(final Movies movies) {
+        if (movies != null) {
+            moviesReviewsAdapter.swapItems(movies.getResults());
+        }
     }
 
     @Override
